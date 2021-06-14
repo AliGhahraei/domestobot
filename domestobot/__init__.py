d46f9ee92ceffe93daca40968601810ccdd95fdd
Ali Ghahraei <aligf94@gmail.com>
2021-06-13 19:58:25 -0500

    Fix import order

--- domestobot/__init__.py
+++ domestobot/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from domestobot._app import (get_app, get_commands_callbacks,
-                             get_groups_callbacks, get_root_dir, get_root_path,
-                             dry_run_option)
+from domestobot._app import (dry_run_option, get_app, get_commands_callbacks,
+                             get_groups_callbacks, get_root_dir, get_root_path)
 
 __all__ = [
     'get_app',
