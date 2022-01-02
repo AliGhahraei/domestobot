1847832b6382e826f43d0719f2d6904a6ee38d93
Ali Ghahraei <aligf94@gmail.com>
2022-01-02 17:47:14 -0600

    Remove packaging requirements as flit will have its own when we migrate

--- dodo.py
+++ dodo.py
@@ -4,7 +4,6 @@ from doit_tools import config, task_compile, task_sort_imports, task_sync
 config.main_requirements_source = 'pyproject.toml'
 config.extra_dependencies = {
     'linting_requirements.txt': [],
-    'packaging_requirements.txt': [],
     'test_requirements.txt': [config.main_requirements_file],
     'typing_requirements.txt': [config.main_requirements_file],
 }
