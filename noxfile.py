75b8d884114b8038624952923bb6825b1fb49033
Ali Ghahraei <aligf94@gmail.com>
2022-01-23 02:14:09 -0600

    Upgrade requirements

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
-from nox_tools import config, linting, tests, typing  # noqa: F401
+from nox_tools import config, linting, tests, typing
 
 config.module = 'domestobot'
+config.sessions = [linting, tests, typing]
